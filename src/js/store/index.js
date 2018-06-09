d229b08b4bdedcf68389b5956ecd0e5e83ccd819
DESKTOP-531B3R1\benjamin <tsyjonlau@gmail.com>
2018-06-09 21:49:16 +0200

    delete Please enter the commit message for your changes. Lines starting

--- src/js/store/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by benjamin on 09/06/2018.
- */
-import { createStore } from "redux";
-import rootReducer from "../reducers/index";
-const store = createStore(rootReducer);
-export default store;
